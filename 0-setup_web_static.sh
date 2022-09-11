26f2c76e82b6b2fdf95166bc50d33d58d57abb83
yora3vs3 <99631524+yora3vs3@users.noreply.github.com>
2022-09-11 13:04:42 +0300

    end

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo "Best School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
@@ -33,3 +33,4 @@ printf %s "server {
 }" > /etc/nginx/sites-available/default
 
 service nginx restart
+
